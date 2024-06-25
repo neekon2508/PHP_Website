010e6658f7219394bbb4c79d5098afd1b00767b6
Nghia Trung <nghia250803@gmail.com>
2024-06-25 12:43:09 +0700

    More secure version uploadFiles.php

--- uploadFiles.php
+++ uploadFiles.php
@@ -14,12 +14,23 @@
     <?php 
     if($_FILES)
     {
+        $valid_type = array("image/jpeg","image/gif","image/png","image/tiff");
         $name = $_FILES['filename']['name'];
-        move_uploaded_file($_FILES['filename']['tmp_name'], "ImageUploadFolder/".$name);
+        if(in_array($_FILES['filename']['type'],$valid_type))
+        {
+            move_uploaded_file($_FILES['filename']['tmp_name'], "ImageUploadFolder/".$name);
     ?>
     <h3>Upload image <?php echo $name ?></h3>
     <img src="<?php echo "ImageUploadFolder/".$name ?>" alt="">
-    <?php } ?>
+    <?php
+      }
+     else
+      {
+    echo "$name is not an accepted image file";
+      }    
+}
+    else echo "No image has been uploaded";
+    ?>
 
 </body>
 </html>
